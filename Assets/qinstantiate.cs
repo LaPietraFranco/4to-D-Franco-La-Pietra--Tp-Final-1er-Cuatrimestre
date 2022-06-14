ff1c9ac784b12d96a58972bef6253770c59f79aa
LaPietraFranco <101827023+LaPietraFranco@users.noreply.github.com>
2022-06-14 12:07:40 -0300

    map finished + instantiate with for

---
+++ Assets/qinstantiate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class qinstantiate : MonoBehaviour
+{
+    GameObject clon;
+    public GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnColiisionEnter(Collision col)
+    {
+        for(int i = 0; i < 12; i++)
+        {
+           clon = Instantiate(prefab);
+            clon.transform.position = transform.position += new Vector3(77.37f, 4f, 1.5f);
+            Destroy(clon, 1);
+        }
+    }
+}
